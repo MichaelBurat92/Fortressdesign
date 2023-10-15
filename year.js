5c6a3d80874f8a6c839d02eb8b3e26b3f1a60350
MichaelBurat92 <141528028+MichaelBurat92@users.noreply.github.com>
2023-10-15 14:45:23 +0200

    Update year.js

--- year.js
+++ year.js
@@ -7,3 +7,10 @@ const von = document.getElementById('von');
 von.innerHTML= currentYear;
 // Das Jahr ausgeben
 console.log("Das aktuelle Jahr ist: " + currentYear);
+// Ein Date-Objekt erstellen
+var currentDate = new Date();
+
+// Das aktuelle Jahr abrufen
+var currentYear2 = currentDate.getFullYear() + 2;
+const bis = document.getElementById('bis');
+bis.innerHTML = currentYear2;
